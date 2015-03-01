5f42bd73da1e97c369e1c8e2aa0c8b27039a9d44
stanchiang <stanchiang23@gmail.com>
2015-02-28 21:17:21 -0800

    basic app logic set up, ready for db logic

--- dbConnector/ViewController.swift
+++ dbConnector/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "login" {
+            let vc: ProfileViewController = segue.destinationViewController as ProfileViewController
+            vc.email = emailField.text
+        } else if segue.identifier == "newuser" {
+            let vc: RegisterViewController = segue.destinationViewController as RegisterViewController
+        }
+    }
 
+    @IBAction func loginAction(sender: UIButton) {
+    }
 
+    @IBAction func newGuyAction(sender: UIButton) {
+    }
 }
 
